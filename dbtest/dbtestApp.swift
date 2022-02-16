3aa264225a477d5a9df9d089ccde3f1c976e6041
action-ukon <actionukon0614mhf@gmail.com>
2022-02-17 00:49:35 +0900

    Initial Commit

---
+++ dbtest/dbtestApp.swift
@@ -0,0 +1,20 @@
+//
+//  dbtestApp.swift
+//  dbtest
+//
+//  Created by 坪田和樹 on 2022/02/17.
+//
+
+import SwiftUI
+
+@main
+struct dbtestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
