d5732c3710cafc983e39cbf46bacecd9c3c0ca88
action-ukon <actionukon0614mhf@gmail.com>
2022-02-23 22:00:44 +0900

    add edit button

--- dbtest/ContentView.swift
+++ dbtest/ContentView.swift
@@ -9,62 +9,69 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.id, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+     @Environment(\.managedObjectContext) private var viewContext
+     
+     @FetchRequest(
+          sortDescriptors: [NSSortDescriptor(keyPath: \Item.id, ascending: true)],
+          animation: .default)
+     private var items: FetchedResults<Item>
      @EnvironmentObject var showingSheet: User
-    
-    var body: some View {
-        VStack{
-            NavigationView {
-                List {
-                    ForEach(items, id: \.self) { item in
-                         //https://www.amazon.co.jp/
-                         let url = URL(string: item.url ?? "")!
-                         if(UIApplication.shared.canOpenURL(url)){
-                              Link(item.name ?? "", destination: url)
-                         }else{
-                              Text(item.name ?? "")
-                                   .foregroundColor(.gray)
-                                   .strikethrough()
-                              + Text("　※リンクが切れています")
+     
+     var body: some View {
+          VStack{
+               NavigationView {
+                    List {
+                         ForEach(items, id: \.self) { item in
+                              //https://www.amazon.co.jp/
+                              let url = URL(string: item.url ?? "")!
+                              if(UIApplication.shared.canOpenURL(url)){
+                                   Link(item.name ?? "", destination: url)
+                              }else{
+                                   Text(item.name ?? "")
+                                        .foregroundColor(.gray)
+                                        .strikethrough()
+                                   + Text("　※リンクが切れています")
+                              }
+                         }.onDelete(perform: deleteItems) .onMove(perform: rowReplace)
+                    }.toolbar {
+                         /// ナビゲーションバーの右に+ボタン配置
+                         ToolbarItem(placement: .navigationBarTrailing) {
+                              Button("+") {
+                                   self.showingSheet.showingSheet.toggle()
+                              }.font(.system(size: 50))
+                                   .sheet(isPresented: $showingSheet.showingSheet) {
+                                        addView()
+                                   }
                          }
-                    }.onDelete(perform: deleteItems)
-                }.toolbar {
-                    /// ナビゲーションバーの右に+ボタン配置
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("+") {
-                            self.showingSheet.showingSheet.toggle()
-                        }.font(.system(size: 50))
-                        .sheet(isPresented: $showingSheet.showingSheet) {
-                            addView()
-                        }
-                        
-                    }
-                }.navigationTitle("お気に入り")
-            }
-        }
-    }
-    
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+                         ToolbarItem(placement: .navigationBarTrailing) {
+                              EditButton()
+                         }
+                    }.navigationTitle("お気に入り")
+               }
+          }
+     }
+     
+     private func deleteItems(offsets: IndexSet) {
+          withAnimation {
+               offsets.map { items[$0] }.forEach(viewContext.delete)
+               do {
+                    try viewContext.save()
+               } catch {
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+               }
+          }
+     }
+     
+     func rowReplace(_ from: IndexSet, _ to: Int) {
+          WebList.move(fromOffsets: from, toOffset: to)
+     }
+     
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-              .environmentObject(User())
-    }
+     static var previews: some View {
+          ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+               .environmentObject(User())
+     }
 }
